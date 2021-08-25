628b553f8e725cdec623e84fc0a64637db121e7c
Denis <denis0001b@gmail.com>
2021-08-25 20:06:51 +0300

    MASTER-1: Example

---
+++ src/HomeWork/HW1.java
@@ -0,0 +1,7 @@
+package HomeWork;
+
+public class HW1 {
+    public static void main (String[] args) {
+        System.out.println("Test number one");
+    }
+}
---
+++ src/HomeWork2/HW1.java
@@ -0,0 +1,7 @@
+package HomeWork2;
+
+public class HW1 {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}
